0bd640bccebcc73a5cba89920c948bb40873a659
Melanie-J-Baker <baker_mel@hotmail.com>
2024-01-15 14:35:40 +0000

    Remove comments

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 const express = require("express");
 const app = express();
-const port = 3000; // You can change this to any port you prefer
+const port = 3000;
 
-// Middleware to serve static files from the public directory
+// Serve static files from the public directory
 app.use(express.static("public"));
 
-// Home Page
+// Home
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/home.html");
 });
 
-// About Page
+// About
 app.get("/about", (req, res) => {
   res.sendFile(__dirname + "/public/about.html");
 });
 
-// Contact Page
+// Contact
 app.get("/contact", (req, res) => {
   res.sendFile(__dirname + "/public/contact.html");
 });
 
-// 404 Page - This should be the last route to catch any undefined routes
+// 404
 app.use((req, res) => {
   res.status(404).sendFile(__dirname + "/public/404.html");
 });
 
-// Start the server
+// start server
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
